1d16949fe71b9785307ee025bc8e2d9f13e0ae59
Cataldo Azzariti <cataldo.azzariti@sapient.com>
2018-10-24 14:53:44 +0100

    trying launching app on docker

--- src/iofileservice/io_file_service.py
+++ src/iofileservice/io_file_service.py
@@ -1,7 +1,17 @@
 from flask import Flask
-from flask_restplus import Api
+from flask_restplus import Api, Resource
 
 api = Api()
 
 app = Flask(__name__)
 api.init_app(app)
+
+
+@api.route('/hello')
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
