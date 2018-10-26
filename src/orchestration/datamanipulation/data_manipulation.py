5fd2b8d99ef36591ea0101679009c02d10aff92c
Cataldo Azzariti <cataldo.azzariti@sapient.com>
2018-10-26 14:19:54 +0100

    added super basic transformations

--- src/orchestration/datamanipulation/data_manipulation.py
+++ src/orchestration/datamanipulation/data_manipulation.py
@@ -1,7 +1,17 @@
 import pandas as pd
 
 
-def transform_data(data):
+def transform_data(df):
+    # Standard-transform a column
+    df['NumberRealEstateLoansOrLines'] = (df['NumberRealEstateLoansOrLines'] - df[
+        'NumberRealEstateLoansOrLines'].mean()) / df['NumberRealEstateLoansOrLines'].std()
 
+    # Create a new feature
+    df['LinesPerAge'] = df['NumberOfOpenCreditLinesAndLoans'] / df['AgeInBank']
 
-    return data
+    # Remove outliers from DebtRatio
+    # Find outliers with std method
+    mask = abs(df['DebtRatio'] - df['DebtRatio'].mean() / df['DebtRatio'].std()) < 3
+    df = df[mask]
+
+    return df
